6c73ea76cb3cacdfa1b57780a4f00d1ef76bfa5e
Grazielle <graziellehortelanazevedo@gmail.com>
2024-03-13 20:02:05 -0300

    api atualizada

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 require('dotenv').config()
 const express = require('express');
+const knex = require("./conexao")
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-return res.json('Api está ok')
+try {
+const carros = await knex('carros')
+return res.json(carros)
+} catch (error) { 
+    console.log(error)
+return res.status(500).json({mensagem: "Erro do servidor"})
+
+}
+
 });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {console.log(`Servidor em pé na porta ${port}`)});
+app.listen(port);
